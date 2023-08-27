d7b40717fd4b3859fd05a3f8792e5426e72d462b
War-Oxi <xowl5460@naver.com>
2023-08-28 02:58:53 +0900

    #14 feature: CompanyEntity 작성

--- src/main/java/com/clone/wanted/Company/Company.java
+++ src/main/java/com/clone/wanted/Company/Company.java
@@ -1,2 +1,32 @@
-package com.clone.wanted.Company;public class Company {
+package com.clone.wanted.Company;
+
+import com.clone.wanted.User.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Company {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "company_Id")
+	private Long companyId;
+
+	@ManyToOne
+	@JoinColumn(name = "userId", referencedColumnName = "userId")
+	private User user;
+
+	@Column(name = "companyName", length = 100)
+	private String companyName;
+
+	@Column(name = "region", length = 100)
+	private String region;
+
+	@Column(name = "address", length = 255)
+	private String address;
+
+	@Column(name = "companyExplanation", length = 255)
+	private String companyExplanation;
 }
