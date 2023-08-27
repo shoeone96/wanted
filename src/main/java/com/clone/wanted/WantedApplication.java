02115afa552bcb8f73cf4e3fd68e49498eb8b547
War-Oxi <xowl5460@naver.com>
2023-08-28 02:43:59 +0900

    #12 feature: UserEntity 작성

--- src/main/java/com/clone/wanted/WantedApplication.java
+++ src/main/java/com/clone/wanted/WantedApplication.java
@@ -2,12 +2,13 @@ package com.clone.wanted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WantedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WantedApplication.class, args);
 	}
-
 }
