86d40d121c616186262fc8aa20bd0a92e3b50b92
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2023-08-27 18:52:22 +0900

    feat: Employment Entity 생성 /#8

---
+++ src/main/java/com/clone/wanted/employment/Employment.java
@@ -0,0 +1,50 @@
+package com.clone.wanted.employment;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name="Employment")
+public class Employment {
+
+    /*Todo 1 develop 브랜치에서  merge 후 Company  FK키 오류 해결되는지 확인하기 */
+    /*Todo 2 BaseEntity로 created_at, updated_at, deleted_at 상속받기 */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="employment_id")
+    private int employmentId;
+
+
+    @ManyToOne
+    @JoinColumn(name="company_id")
+    private Company company;
+
+    @Column(name="employment_title")
+    private String employmentTitle;
+
+    @Column(name="employment_contents")
+    private String employmentContents;
+
+    private String address;
+
+    private LocalDateTime deadline;
+
+    @Column(name="employment_reward")
+    private int employmentReward;
+
+    private String position;
+
+
+}
