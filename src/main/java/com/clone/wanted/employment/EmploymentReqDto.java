14f2b403a2ad810d5e361cb69f34699df457e767
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2023-08-31 16:28:46 +0900

    #3 feature: employment Res Req DTO 생성

---
+++ src/main/java/com/clone/wanted/employment/EmploymentReqDto.java
@@ -0,0 +1,27 @@
+package com.clone.wanted.employment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class EmploymentReqDto {
+
+    private long companyId;
+    private String employmentTitle;
+    private String address;
+    private String deadline;
+    private int applicantReward;
+    private int recommenderReward;
+
+    // 채용공고 수정시 사용되는 생성자
+    public EmploymentReqDto(String employmentTitle, String address, String deadline, int applicantReward, int recommenderReward) {
+        this.employmentTitle = employmentTitle;
+        this.address = address;
+        this.deadline = deadline;
+        this.applicantReward = applicantReward;
+        this.recommenderReward = recommenderReward;
+    }
+}
