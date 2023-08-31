55a4179cd4fced3094b18128d9569113519d5b2c
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2023-08-31 16:36:29 +0900

    #3 feature: EmploymentRepository 생성

---
+++ src/main/java/com/clone/wanted/employment/EmploymentRepository.java
@@ -0,0 +1,6 @@
+package com.clone.wanted.employment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmploymentRepository extends JpaRepository<Employment,Long> {
+}
