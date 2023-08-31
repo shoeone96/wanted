45e5789cf4aedbe6f09dcc7dd87240387317fdf7
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2023-08-31 18:26:18 +0900

    #3 feature: 채용공고 상세조회 및 전체조회 기능 구현

--- src/main/java/com/clone/wanted/employment/EmploymentController.java
+++ src/main/java/com/clone/wanted/employment/EmploymentController.java
@@ -3,10 +3,9 @@ package com.clone.wanted.employment;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +19,22 @@ public class EmploymentController {
     public ResponseEntity createEmployment(@RequestBody EmploymentReqDto employmentReqDto) {
         employmentService.createEmployment(employmentReqDto);
         return ResponseEntity.ok(null);
+    }
+
+    //채용 공고 상세조회
+    /**Todo 좋아요 수 컬럼으로 추가해서 반환하기*/
+    @GetMapping("/employments/{employmentId}")
+    public ResponseEntity<EmploymentResDto> retrieveEmployment(@PathVariable long employmentId) {
+        EmploymentResDto employmentResDto = employmentService.retrieveEmployment(employmentId);
+        return ResponseEntity.ok(employmentResDto);
+    }
+
+    //채용 공고 전체조회
+    /**Todo 좋아요 수 컬럼으로 추가해서 반환하기*/
+    @GetMapping("/employments")
+    public ResponseEntity<List<EmploymentResDto>> retrieveAllEmployment() {
+        List<EmploymentResDto> employmentResDtoList = employmentService.retrieveAllEmployment();
+        return ResponseEntity.ok(employmentResDtoList);
     }
 
 
--- src/main/java/com/clone/wanted/employment/EmploymentService.java
+++ src/main/java/com/clone/wanted/employment/EmploymentService.java
@@ -4,8 +4,14 @@ import com.clone.wanted.Company.Company;
 import com.clone.wanted.Company.CompanyRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,6 +21,7 @@ public class EmploymentService {
     private final EmploymentRepository employmentRepository;
     private final CompanyRepository companyRepository;
 
+    //채용공고 생성
     public void createEmployment (EmploymentReqDto employmentReqDto){
         long companyId = employmentReqDto.getCompanyId();
         Company company = companyRepository.findById(companyId).get();
@@ -22,6 +29,33 @@ public class EmploymentService {
         Employment employment = new Employment(company, employmentReqDto);
 
         employmentRepository.save(employment);
-
     }
+    //채용공고 상세 조회
+    public EmploymentResDto retrieveEmployment(@PathVariable long employmentId) {
+        Employment employment = employmentRepository.findById(employmentId).get();
+        Company company = employment.getCompany();
+
+        EmploymentResDto employmentResDto = new EmploymentResDto(employment, company);
+
+        return employmentResDto;
+    }
+
+    //채용공고 전체 조회
+    public List<EmploymentResDto> retrieveAllEmployment() {
+        List<Employment> employmentList = employmentRepository.findAll();
+
+        //employmentList employmentsResDtoList로 변환하기
+        ArrayList<EmploymentResDto> employmentResDtoList = new ArrayList<>();
+
+        for(int i=0;i<employmentList.size();i++){
+            Employment employment = employmentList.get(i);
+            Company company = employment.getCompany();
+            employmentResDtoList.add(new EmploymentResDto(employment,company));
+        } //변환 끝
+
+        return employmentResDtoList;
+    }
+
+
+
 }
