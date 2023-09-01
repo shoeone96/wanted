e3bb85068c40dc28422b003c7e95765d795200f1
shoeone96 <dlwnddnjs96@gmail.com>
2023-09-01 18:01:32 +0900

    #34 feat: front에 지원 날짜 전달하기 위한 getter 작성

--- src/main/java/com/clone/wanted/BaseEntity.java
+++ src/main/java/com/clone/wanted/BaseEntity.java
@@ -3,12 +3,14 @@ package com.clone.wanted;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
