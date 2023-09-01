5da132e2cf1a1cd8a3a30293bf77ab501fd9f274
shoeone96 <dlwnddnjs96@gmail.com>
2023-09-01 16:34:24 +0900

    #34 feat: 새롭게 지원 등록을 위한 생성자 추가

--- src/main/java/com/clone/wanted/application/Application.java
+++ src/main/java/com/clone/wanted/application/Application.java
@@ -4,13 +4,14 @@ import com.clone.wanted.BaseEntity;
 import com.clone.wanted.User.User;
 import com.clone.wanted.employment.Employment;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Application extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,5 +25,15 @@ public class Application extends BaseEntity {
     private Employment employment;
     @Enumerated(EnumType.STRING)
     private ApplicationStatus applicationStatus;
+
+    private Application (User user, Employment employment){
+        this.user = user;
+        this.employment = employment;
+        this.applicationStatus = ApplicationStatus.ONGOING;
+    }
+
+    public static Application newEnrollment(User user, Employment employment){
+        return new Application(user, employment);
+    }
 
 }
