c5882dbc45fdeba350dc17f75f45a89df97e8c0e
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2023-09-01 14:36:23 +0900

    #23 fix: Exception 예외처리 중 상태 코드가 200인 에러 해결

--- src/main/java/com/clone/wanted/GlobalExceptionHandler.java
+++ src/main/java/com/clone/wanted/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static com.clone.wanted.config.BaseResponseStatus.*;
@@ -23,9 +24,10 @@ public class GlobalExceptionHandler {
 
     //예외 전역 처리
     /*TODO 로그 파일 생성하기*/
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = Exception.class)
     public BaseResponse ExceptionHandler(Exception e) {
-        log.error("MethodArgumentNotValidException", e);
+        //log.error("MethodArgumentNotValidException", e);
         return new BaseResponse(GlOBAL_EXCEPTION);
     }
 
