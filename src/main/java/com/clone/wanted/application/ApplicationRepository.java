0b677037b5da8d4a50f9e7fa5cae968bba175a6f
shoeone96 <dlwnddnjs96@gmail.com>
2023-09-01 18:03:09 +0900

    #34 feat: 유저가 본인이 지원한 공고를 조회하는 코드 작성

--- src/main/java/com/clone/wanted/application/ApplicationRepository.java
+++ src/main/java/com/clone/wanted/application/ApplicationRepository.java
@@ -5,10 +5,12 @@ import com.clone.wanted.employment.Employment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 interface ApplicationRepository extends JpaRepository<Application, Long> {
 
     Optional<Application> findByUserAndEmployment(User suer, Employment employment);
+    List<Application> findAllByUser(User user);
 }
