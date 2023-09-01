f3cd4148deb54e12f9970e9806402622d069782b
shoeone96 <dlwnddnjs96@gmail.com>
2023-09-01 16:37:07 +0900

    #34 공고지원에 필요한 데이터 가져오는 repository 작성

---
+++ src/main/java/com/clone/wanted/application/ApplicationRepository.java
@@ -0,0 +1,14 @@
+package com.clone.wanted.application;
+
+import com.clone.wanted.User.User;
+import com.clone.wanted.employment.Employment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+    Optional<Application> findByUserAndEmployment(User suer, Employment employment);
+}
