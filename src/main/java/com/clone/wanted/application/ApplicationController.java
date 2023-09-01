84d60beb9a806563925999ac3d88ed5a08dfecb2
shoeone96 <dlwnddnjs96@gmail.com>
2023-09-01 17:15:02 +0900

    #34 지원 status를 변경하는 api 작성

--- src/main/java/com/clone/wanted/application/ApplicationController.java
+++ src/main/java/com/clone/wanted/application/ApplicationController.java
@@ -1,5 +1,6 @@
 package com.clone.wanted.application;
 
+import com.clone.wanted.application.requestDto.EstimateRequestDto;
 import com.clone.wanted.config.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,17 @@ public class ApplicationController {
     /**
      * @param email
      * @return BaseResponse
-     * TODO: spring security 단 이후 Authentication 으로 변경
+     * TODO: spring security 단 이후 email 을 Authentication 으로 변경
      */
     @PostMapping("api/v1/applications/{employmentId}")
     public BaseResponse<Void> enroll(@RequestBody String email, @PathVariable Long employmentId) {
         applicationService.enroll(email, employmentId);
+        return BaseResponse.success();
+    }
+
+    @PatchMapping("api/v1/applications/{employmentId}/estimate")
+    public BaseResponse<Void> estimate(@RequestBody String email, @RequestBody EstimateRequestDto requestDto, @PathVariable Long employmentId){
+        applicationService.estimate(email, requestDto, employmentId);
         return BaseResponse.success();
     }
 }
