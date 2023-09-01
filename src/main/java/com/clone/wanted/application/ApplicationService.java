a41785adafefeec32df2b08856b13762d2876f2b
shoeone96 <dlwnddnjs96@gmail.com>
2023-09-01 17:15:27 +0900

    #34 지원 상태를 변경하는 서비스 로직 작성

--- src/main/java/com/clone/wanted/application/ApplicationService.java
+++ src/main/java/com/clone/wanted/application/ApplicationService.java
@@ -2,6 +2,7 @@ package com.clone.wanted.application;
 
 import com.clone.wanted.User.User;
 import com.clone.wanted.User.UserRepository;
+import com.clone.wanted.application.requestDto.EstimateRequestDto;
 import com.clone.wanted.config.BaseException;
 import com.clone.wanted.config.BaseResponseStatus;
 import com.clone.wanted.employment.Employment;
@@ -22,11 +23,21 @@ public class ApplicationService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.USER_NOT_FOUND));
         Employment employment = employmentRepository.findById(employmentId)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.Employment_NOT_FOUND));
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.EMPLOYMENT_NOT_FOUND));
         applicationRepository.findByUserAndEmployment(user, employment)
                 .ifPresent((application -> {
                     throw new BaseException(BaseResponseStatus.APPLICATION_ALREADY_EXIST);
                 }));
         applicationRepository.save(Application.newEnrollment(user, employment));
+    }
+
+    public void estimate(String email, EstimateRequestDto requestDto, Long employmentId) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.USER_NOT_FOUND));
+        Employment employment = employmentRepository.findById(employmentId)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.EMPLOYMENT_NOT_FOUND));
+        Application application = applicationRepository.findByUserAndEmployment(user, employment)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.EMPLOYMENT_NOT_FOUND));
+        application.updateStatus(ApplicationStatus.returnStatus(requestDto.getApplicationStatus()));
     }
 }
