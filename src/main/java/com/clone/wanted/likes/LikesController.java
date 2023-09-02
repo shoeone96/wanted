5d48fb308eb80e1cd96758ae76530ffd063c4c47
shoeone96 <dlwnddnjs96@gmail.com>
2023-09-03 02:45:56 +0900

    fix: authentication 객체

--- src/main/java/com/clone/wanted/likes/LikesController.java
+++ src/main/java/com/clone/wanted/likes/LikesController.java
@@ -4,6 +4,7 @@ package com.clone.wanted.likes;
 import com.clone.wanted.config.BaseResponse;
 import com.clone.wanted.likes.responseDto.LikesResDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,23 +14,14 @@ public class LikesController {
     private final LikesService likesService;
 
     @PostMapping("/employment/{employmentId}/likes")
-    public BaseResponse<Void> changeLikes(@PathVariable long employmentId){
-        //Todo userId 나중에 PathVariable로 넘어올 것임 , 그 때 변경
-        //UserId 임시값
-        long userId=3;
-        likesService.changeLikes(employmentId,userId);
-
+    public BaseResponse<Void> changeLikes(Authentication authentication, @PathVariable long employmentId){
+        likesService.changeLikes(employmentId,authentication.getName());
         return BaseResponse.success();
     }
 
     @GetMapping("/employment/{employmentId}/likes")
-    public BaseResponse<LikesResDto> retrieveLikes(@PathVariable long employmentId){
-        //Todo userId 나중에 PathVariable로 넘어올 것임 , 그 때 변경
-        //UserId 임시값
-        long userId=1;
-        LikesResDto likesResDto = likesService.retrieveLikes(employmentId, userId);
-
-        return BaseResponse.success(likesResDto);
+    public BaseResponse<LikesResDto> retrieveLikes(Authentication authentication, @PathVariable long employmentId){
+        return BaseResponse.success(likesService.retrieveLikes(employmentId, authentication.getName()));
     }
 
 }
