e775aae565657deea222e5cee8762bb022926456
KKam._.Ji <xowl5460@naver.com>
2023-09-03 22:31:11 +0900

    #61 feature 좋아요 전체 조회 기능 구현

--- src/main/java/com/clone/wanted/likes/LikesController.java
+++ src/main/java/com/clone/wanted/likes/LikesController.java
@@ -1,11 +1,15 @@
 package com.clone.wanted.likes;
 
 
+import com.clone.wanted.application.responseDto.CompanyApplicationResponseDto;
 import com.clone.wanted.config.BaseResponse;
+import com.clone.wanted.likes.responseDto.LikesDto;
 import com.clone.wanted.likes.responseDto.LikesResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +23,18 @@ public class LikesController {
         return BaseResponse.success();
     }
 
-@GetMapping("/employment/{employmentId}/likes")
-public BaseResponse<LikesResDto> retrieveLikes(@PathVariable long employmentId){
-    return BaseResponse.success(likesService.retrieveLikes(employmentId));
-}
+    @GetMapping("/employment/{employmentId}/likes")
+    public BaseResponse<LikesResDto> retrieveLikes(@PathVariable long employmentId){
+        return BaseResponse.success(likesService.retrieveLikes(employmentId));
+    }
+
+    //test -> 좋아요 누른 회원 정보 조회
+    @GetMapping("/likes")
+    public BaseResponse<List<LikesDto>> retrieveLikeAll() {
+        List<LikesDto> likesList = likesService.retrieveLikeAll();
+
+        return BaseResponse.success(likesList);
+    }
 
 }
 
